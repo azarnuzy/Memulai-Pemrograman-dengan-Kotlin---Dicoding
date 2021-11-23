46b3ae99d334990bbb5ffc4428dcaa9c939ff8dd
azarnuzy <azarnuzy@gmail.com>
2021-11-23 19:30:07 +0700

    learning lambda

---
+++ src/main/kotlin/com/dicoding/kotlin/25Lambda.kt
@@ -0,0 +1,11 @@
+package com.dicoding.kotlin
+
+val message = { println("Hello From Lambda")}
+val printMessage = {message: String -> println(message)}
+val messageLength = {message: String -> message.length}
+fun main() {
+    message()
+    printMessage("Hello From Lambda2")
+    val length = messageLength("Hello From Lambda")
+    println("Message Length = $length")
+}
