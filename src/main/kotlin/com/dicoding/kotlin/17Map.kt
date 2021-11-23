100a5b3eb14c4ac623e0b8b7bf72e8c631b6e7d8
azarnuzy <azarnuzy@gmail.com>
2021-11-23 08:06:11 +0700

    learning map

---
+++ src/main/kotlin/com/dicoding/kotlin/17Map.kt
@@ -0,0 +1,24 @@
+package com.dicoding.kotlin
+
+fun main() {
+    val capital = mapOf(
+        "Jakarta" to "Indonesia",
+        "London" to "England",
+        "New Delhi" to "India"
+    )
+
+    println(capital["Jakarta"])
+    println(capital.getValue("Jakarta"))
+
+    val mapKeys = capital.keys
+    val mapValues = capital.values
+
+    println(mapKeys)
+    println(mapValues)
+
+    val mutableCapital = capital.toMutableMap()
+    mutableCapital.put("Amsterdam", "Netherlands")
+    mutableCapital.put("Berlin", "Germany")
+
+    println(mutableCapital)
+}
