b77bc61aa71f1c9c51497dae339845d19a24f6a8
azarnuzy <azarnuzy@gmail.com>
2021-11-23 08:25:22 +0700

    learning sequences

---
+++ src/main/kotlin/com/dicoding/kotlin/19Sequences.kt
@@ -0,0 +1,12 @@
+package com.dicoding.kotlin
+
+fun main() {
+    val list = (1..1000000).toList()
+//    list.filter {it % 5 == 0}.map {it * 2}.forEach{println(it)}
+//    list.asSequence().filter { it % 5 == 0 }.map { it + it }.forEach { println(it) }
+
+    val sequenceNumber = generateSequence(1) { it + 1 }
+    sequenceNumber.take(5).forEach { print("$it ") }
+
+
+}
