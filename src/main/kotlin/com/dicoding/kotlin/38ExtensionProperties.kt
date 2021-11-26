ec708f118ccf67e6c4ce597b90c873222af50d5b
azarnuzy <azarnuzy@gmail.com>
2021-11-26 15:04:10 +0700

    learning about extension properties

---
+++ src/main/kotlin/com/dicoding/kotlin/38ExtensionProperties.kt
@@ -0,0 +1,12 @@
+package com.dicoding.kotlin
+
+fun main() {
+    val dicodingCat = Animal4("Dicoding Miaw", 5.0, 2, true)
+    println(dicodingCat.getAnimalInfo)
+}
+
+class Animal4(var name: String, var weight: Double, var age: Int, var isMammal: Boolean)
+
+val Animal4.getAnimalInfo : String
+    get() = "Nama: ${this.name}, Berat: ${this.weight}, Umur: ${this.age}, Mamalia: ${this.isMammal}"
+
